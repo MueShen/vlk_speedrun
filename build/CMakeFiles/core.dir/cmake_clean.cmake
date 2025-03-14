af8ba64822166ce612211b27287b87ab7f2e2557
mushen <mueshen.sh@gmail.com>
2025-03-14 20:22:23 +0300

    Attempting structuring folders, cmake and linkin' shi

---
+++ build/CMakeFiles/core.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "'/home/mushen/Code/vlk_speedrun/bin'/core"
+  "'/home/mushen/Code/vlk_speedrun/bin'/core.pdb"
+  "CMakeFiles/core.dir/src/main.cpp.o"
+  "CMakeFiles/core.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
